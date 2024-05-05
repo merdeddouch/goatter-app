280797d764cfb7d81ae9c1f6910dcbce8a98fa2d
primo <sim.merdedouch@gmail.com>
2024-05-05 13:11:13 +0100

    slideBar component and slideBarOption component

---
+++ src/Components/Slidbar/Slidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Grid, Stack} from "@mui/material";
+import TwitterIcon from '@mui/icons-material/Twitter';
+function Slidebar(props) {
+    return (
+        <Stack spacing={2} direction="column">
+            <TwitterIcon></TwitterIcon>
+            
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+            {/*slideBarOptions*/}
+
+            {/*MainButtonToAddNewGoat_thread*/}
+        </Stack>
+    );
+}
+
+export default Slidebar;
