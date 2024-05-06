1bb7a756ccc9e6bac17282708a0fd26cb181000d
primo <sim.merdedouch@gmail.com>
2024-05-06 22:37:16 +0100

    added a leftborder to sideBar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from './Components/Main/Main';
 function App() {
   return (
     <Grid container  className="App">
-      <Grid item m={3} md={3}>
+      <Grid item m={3} md={2} sx={{ borderRight: '2px solid #E1E8ED', paddingLeft: '10px', width:"400px" }}>
     {/*  slide bar  */}
     <Slidebar>
     </Slidebar>
