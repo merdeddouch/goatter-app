749ada6f47ac703337da48cb151c42aebc08d662
primo <sim.merdedouch@gmail.com>
2024-05-07 22:27:34 +0100

    coded the PostPost

---
+++ src/Components/Post/PostProfil/PostProfil.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Avatar } from '@mui/material';
+
+const PostProfil = () => {
+  return (
+    <Avatar
+      sx={{ width: 35, height: 35 }}
+      alt="Lofi"
+      src={"/assets/static/images/lofi.jpg"}
+    />
+  );
+}
+
+export default PostProfil;
