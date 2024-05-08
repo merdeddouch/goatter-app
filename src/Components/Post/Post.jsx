279bc08ef983f85f99eeb1ccf69acca3378d3844
primo <sim.merdedouch@gmail.com>
2024-05-08 18:33:55 +0100

    fixed some style flex:1

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './postStyle.css'
-import { Box, Card, CardActions, CardContent, CardHeader, CardMedia, Stack, Typography } from '@mui/material'
+import { Box, Card, CardActions, CardContent, CardHeader, CardMedia, Stack, Typography,IconButton } from '@mui/material'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import PostProfil from './PostProfil/PostProfil'
 const Post = () => {
   return (
 <div>
     <Card>
     <CardContent>
-    <Stack direction={'row'}>
-        <Box display={"flex"} gap={2} alignItems={"center"}>
+    <Stack  direction={'row'}>
+        <Box display={"flex"} sx={{
+            marginRight:"auto",
+        }} gap={2} alignItems={"center"}>
             <PostProfil></PostProfil>
             <div className="post_header_info">
             <Typography className='name' variant='subtitle1'>
@@ -19,6 +22,11 @@ const Post = () => {
             </p>
             </div>
         </Box>
+        <IconButton color={"primary"} size={"small"}>
+            <MoreHorizIcon>
+
+            </MoreHorizIcon>
+        </IconButton>
         </Stack>       
     </CardContent>
         <CardMedia>
