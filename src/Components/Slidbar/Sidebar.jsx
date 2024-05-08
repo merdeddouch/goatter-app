808837b12db08f0874fb4a7c3dc16781de913a71
primo <sim.merdedouch@gmail.com>
2024-05-08 13:10:08 +0100

    added useEffect to handle size

--- src/Components/Slidbar/Sidebar.jsx
+++ src/Components/Slidbar/Sidebar.jsx
@@ -1,45 +1,64 @@
-import React, { useState } from 'react';
-import {Box, Button, IconButton, Stack} from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import { Box, Button, IconButton } from "@mui/material";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SlidebarOption_v2 from '../SidebarOption/SidebarOption_v2';
-import '../../helper/options'
 import { options } from '../../helper/options';
-import './SlidebarStyle.css'
+import './SlidebarStyle.css';
+
 function Slidebar(props) {
-    const [activeButton,setActiveButton] = useState(0);
+    const [activeButton, setActiveButton] = useState(0);
+    const [screenWidth, setScreenWidth] = useState();
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove the event listener when component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const handlClick = (e) => {
-        //to check console.log(e.target.id);
-        const currentActive = e.target.id
+        const currentActive = e.target.id;
         setActiveButton(currentActive);
-    }
+    };
+
+    const isSlimSidebar = screenWidth < 1300; // Remove quotes around 900px
 
     return (
-        <div className='sideBar'>
-          <Box >
-            <IconButton id={0} size='large' color='primary' onClick={handlClick}>
-                <TwitterIcon></TwitterIcon>
-            </IconButton>
+        <div className={isSlimSidebar ? "slimSideBar" : "sideBar"}>
+            <Box>
+                <IconButton id={0} color='primary' onClick={handlClick}>
+                    <TwitterIcon />
+                </IconButton>
             </Box>
-            
-            {/*slideBarOptions*/}
-            {
-                options.map((op,index) => {
-                    return <SlidebarOption_v2 handlClick={handlClick} activeButton={activeButton} option={op} key={index} id={index}>
-                    </SlidebarOption_v2>
-                })
-            }
 
-            <Button size='medium' variant='contained'
+            {/*slideBarOptions*/}
+            {options.map((op, index) => (
+                <SlidebarOption_v2
+                    handlClick={handlClick}
+                    activeButton={activeButton}
+                    option={op}
+                    key={index}
+                    id={index}
+                />
+            ))}
+
+            <Button
+                className={"tweetButton"}
+                variant='contained'
                 sx={{
-                    borderRadius:"25px",
-                    width:"11rem",
-                    backgroundColor:"#1DA1F0"
+                    borderRadius: "25px",
+                    width: "11rem",
+                    backgroundColor: "#1DA1F0"
                 }}
             >
                 POST
             </Button>
         </div>
-          
     );
 }
 
