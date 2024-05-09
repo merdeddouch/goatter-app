1ca7d2c7499339df2719ae7bbc7756d800319b24
primo <sim.merdedouch@gmail.com>
2024-05-09 23:53:04 +0100

    implement the notifiaction shape

--- src/Components/Notifications/OneNotification.jsx
+++ src/Components/Notifications/OneNotification.jsx
@@ -1,10 +1,43 @@
+import { IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
+import PostProfil from '../Post/PostProfil/PostProfil'
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import { MoreHorizOutlined } from '@mui/icons-material';
+import './oneNotificationStyle.css'
 
 const OneNotification = () => {
   return (
+<Stack direction={"row"} className='oneNotification'>
     <div>
-      
+    <LocalFireDepartmentIcon sx={{
+        height:"30px",
+        width:"30px",
+    }}>
+    </LocalFireDepartmentIcon>
     </div>
+    <Stack direction={"column"} sx={{
+        width:"100%",
+    }} >
+    <Stack direction={"row"} justifyContent={"space-between"}>
+        <PostProfil></PostProfil>
+        <IconButton size='small' color='primary'>
+            <MoreHorizOutlined>
+            </MoreHorizOutlined>
+        </IconButton>
+    </Stack>
+    <Stack direction={"column"}>
+        <Typography variant='body1' gutterBottom>
+            Recent post from <strong>Lofi</strong>
+        </Typography>
+        <Typography variant='body2' color='sconedry'>
+            HCKJDSHFKJDSHKJGDSFJHG SDFKHDSFJHGJ GJHFDS
+            HFSDHKHJSFD KJHSDFLKJHDSF LSHDFKJHFDSKJHSDFKJH 
+            FDSLLSDHDSKJH JKHSDFKJHDSF FDS JHKSDFHKSDJFH KJSHFDKJHDSF
+        </Typography>
+    </Stack>
+    </Stack>
+
+</Stack>
   )
 }
 
