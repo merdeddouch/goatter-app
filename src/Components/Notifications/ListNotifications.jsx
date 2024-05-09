a3467251bb064d7ff601cc0776d6fd0c504ee930
primo <sim.merdedouch@gmail.com>
2024-05-09 23:54:09 +0100

    implement list of notifications

---
+++ src/Components/Notifications/ListNotifications.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import OneNotification from './OneNotification'
+
+const ListNotifications = () => {
+  return (
+<>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+<OneNotification></OneNotification>
+</>
+  )
+}
+
+export default ListNotifications
