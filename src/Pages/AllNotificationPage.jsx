f9b4daec11857c45888d18ce02a1cdb4f6b2f45e
primo <sim.merdedouch@gmail.com>
2024-05-12 00:10:05 +0100

    import the NotificationsHeader and ListNotification to allNotificationPage

---
+++ src/Pages/AllNotificationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NotificationHeader from '../Components/Header/NotificationHeader'
+import ListNotifications from '../Components/Notifications/ListNotifications'
+const AllNotificationPage = () => {
+  return (
+    <>
+      <NotificationHeader></NotificationHeader>
+      <ListNotifications></ListNotifications> 
+    </>
+  )
+}
+
+export default AllNotificationPage
