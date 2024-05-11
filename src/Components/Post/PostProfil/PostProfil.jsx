0ddfcf8e7592df7d57518fd64607c4f55bf20d9d
primo <sim.merdedouch@gmail.com>
2024-05-11 23:28:54 +0100

    modified profile dimension

--- src/Components/Post/PostProfil/PostProfil.jsx
+++ src/Components/Post/PostProfil/PostProfil.jsx
@@ -4,7 +4,7 @@ import { Avatar } from '@mui/material';
 const PostProfil = () => {
   return (
     <Avatar
-      sx={{ width: 35, height: 35 }}
+      sx={{ width: 40, height: 40 }}
       alt="Lofi"
       src={"/assets/static/images/naruto.jpg"}
     />
