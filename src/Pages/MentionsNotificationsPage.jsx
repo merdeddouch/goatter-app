d2376b3f9990497aaea434efd87783d5ce89f566
primo <sim.merdedouch@gmail.com>
2024-05-13 00:41:34 +0100

    we added mention*Notification page

---
+++ src/Pages/MentionsNotificationsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+const MentionsNotificationsPage = () => {
+  return (
+    <>    
+      <Typography textAlign={'center'} marginTop={10} variant='h4' fontWeight={"bold"}>
+         There is no mentions Notification yet ...
+      </Typography>
+    </>
+  )
+}
+
+export default MentionsNotificationsPage
