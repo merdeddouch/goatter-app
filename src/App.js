f12b416c6eb60a333aba5c147c0a73aa27072c37
primo <sim.merdedouch@gmail.com>
2024-05-13 00:29:44 +0100

    Adding changes The changes are: 1-we added the brower router 2-after that, we added the context provider to share state with components.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import Slidebar from './Components/Slidbar/Sidebar';
 import Main from './Components/Main/Main';
 import Home from './Pages/HomePage';
+import { BrowserRouter } from 'react-router-dom';
+import { buttonBackToHomeContext } from './Context/buttonHomeContext';
+import { useState } from 'react';
 
 function App() {
+  const [activeButton, setActiveButton] = useState(0);
   return (
-    <Grid container  className="App">
-       <Grid item md={2} sx={{ borderRight: '2px solid #E1E8ED', paddingRight: '10px',}}>
+    <BrowserRouter>
+      <Grid container  className="App">
+      <buttonBackToHomeContext.Provider value={{
+        activeButton:activeButton,
+        setActiveButton:setActiveButton
+
+      }}>
+      <Grid item md={2} sx={{ borderRight: '2px solid #E1E8ED', paddingRight: '10px',}}>
     {/*  slide bar  */}
     <Slidebar>
     </Slidebar>
     </Grid>
         {/*main*/}
     <Grid item md={6} sx={{ flex:1,}} >
-        <Home></Home>
+        <Main></Main>
     </Grid>
+    </buttonBackToHomeContext.Provider>
         {/*widgets*/}
-      
     </Grid>
+    </BrowserRouter>
   );
 }
 export default App;
