dc88df28fbd43b31d4e5ed08ecac5d581bc4865f
primo <sim.merdedouch@gmail.com>
2024-05-13 00:50:19 +0100

    create a list of posts

---
+++ src/Components/Post/ListPost.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Post from './Post'
+import HomeHeader from '../Header/HomeHeader'
+const ListPost = () => {
+  return (
+    <>
+    <HomeHeader></HomeHeader>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
+    </>
+  )
+}
+
+export default ListPost
