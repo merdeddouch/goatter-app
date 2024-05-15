6bfa36ac251014f44258f769331a8442aa8b8ccf
primo <sim.merdedouch@gmail.com>
2024-05-15 14:27:50 +0100

    import postFrom

--- src/Components/Post/ListPost.jsx
+++ src/Components/Post/ListPost.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Post from './Post'
 import HomeHeader from '../Header/HomeHeader'
+import PostForm from './PostForm'
 const ListPost = () => {
   return (
     <>
     <HomeHeader></HomeHeader>
+    <PostForm></PostForm>
     <Post></Post>
     <Post></Post>
     <Post></Post>
