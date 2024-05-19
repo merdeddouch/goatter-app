96ded43167703d9ecf9070199d0cfa00d629a6c6
primo <sim.merdedouch@gmail.com>
2024-05-19 19:49:17 +0100

    added the context provider and move the states to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { Grid } from '@mui/material';
+import { Button, Grid, IconButton } from '@mui/material';
 import './App.css';
 import Slidebar from './Components/Slidbar/Sidebar';
 import Main from './Components/Main/Main';
 import Home from './Pages/HomePage';
 import { BrowserRouter } from 'react-router-dom';
 import { buttonBackToHomeContext } from './Context/buttonHomeContext';
+import { popupContext } from './Context/popup';
 import { useState } from 'react';
+import TestEmoji from './TestComponents/TestEmoji';
+import Popup_media from './Components/Popup_media';
+import ClearIcon from '@mui/icons-material/Clear';
 
 function App() {
   const [activeButton, setActiveButton] = useState(0);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [popupUrl, setPopupUrl] = useState('');
   return (
+    // <TestEmoji></TestEmoji>
     <BrowserRouter>
       <Grid container  className="App">
+      <popupContext.Provider value={{
+        popupOpen:popupOpen,
+        setPopupOpen:setPopupOpen,
+        popupUrl:popupUrl,
+        setPopupUrl:setPopupUrl
+      }}>
       <buttonBackToHomeContext.Provider value={{
         activeButton:activeButton,
         setActiveButton:setActiveButton
@@ -28,6 +41,22 @@ function App() {
     </Grid>
     </buttonBackToHomeContext.Provider>
         {/*widgets*/}
+        {
+          popupOpen && <div  className="popup" >
+          <IconButton sx={{
+            position:"absolute",
+            top:0,
+            right:"10px",
+            color:"white",
+          }}  size='large' onClick={() => setPopupOpen(false)}>
+            <ClearIcon sx={{
+              fontSize:'40px'
+            }}> </ClearIcon>
+          </IconButton>
+          <Popup_media url={popupUrl} />
+          </div>
+        }
+    </popupContext.Provider>
     </Grid>
     </BrowserRouter>
   );
